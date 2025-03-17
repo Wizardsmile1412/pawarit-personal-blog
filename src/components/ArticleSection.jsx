f3054d11c86ad6bae018d517c8f99f6cab0c15a2
Tor <p.sripayom@gmail.com>
2025-03-17 18:52:07 +0700

    feat(blogCard): add initial draft of blogCard article which render in Article section

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -10,18 +10,49 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
+  import { blogPosts } from '@/data/blogPosts';
 
+  function BlogCard({image, category, title, description, author, date}) {
+    return (
+      <div className="flex flex-col gap-4">
+        <a href="#" className="relative h-[212px] sm:h-[360px]">
+          <img className="w-full h-full object-cover rounded-md" src={image} alt={title}/>
+        </a>
+        <div className="flex flex-col">
+          <div className="flex">
+            <span className="bg-green-200 rounded-full px-3 py-1 text-sm font-semibold text-green-600 mb-2">{category}
+            </span>
+          </div>
+          <a href="#" >
+            <h2 className="text-start font-bold text-xl mb-2 line-clamp-2 hover:underline">
+            {title}
+            </h2>
+          </a>
+          <p className="text-muted-foreground text-sm mb-4 flex-grow line-clamp-3">
+          {description}</p>
+          <div className="flex items-center text-sm">
+            <img className="w-8 h-8 rounded-full mr-2" src="https://res.cloudinary.com/dcbpjtd1r/image/upload/v1728449784/my-blog-post/xgfy0xnvyemkklcqodkg.jpg" alt="Tomson P." />
+            <span>{author}</span>
+            <span className="mx-2 text-gray-300">|</span>
+            <span>{date}</span>
+          </div>
+        </div>
+      </div>
+    );
+   }
+   
 
 const categories = ["Highlight", "Coffee", "Inspiration", "General"];
 function ArticleSection() {
   const [selectedCategory, setSelectedCategory] = useState("Highlight");
 
   return (
-    <div className="py-6 px-20">
-      <h2 className="text-xl font-bold text-gray-900 mb-8">Latest articles</h2>
+    <div className="my-20 px-20">
+
+      <h2 className="text-xl font-bold text-gray-900">Latest articles</h2>
 
       {/* Responsive Container */}
-      <div className=" bg-[#EFEEEB] rounded-lg p-4 flex flex-col space-y-4 sm:h-20 sm:flex-row sm:space-y-0 sm:justify-between sm:items-center">
+      <div className=" bg-[#EFEEEB] rounded-lg p-4 my-10 flex flex-col space-y-4 sm:h-20 sm:flex-row sm:space-y-0 sm:justify-between sm:items-center">
         
         {/* Categories - Buttons on Desktop, Dropdown on Mobile */}
         <div className="w-full sm:w-auto">
@@ -65,6 +96,60 @@ function ArticleSection() {
         </Select>
         
       </div>
+
+      {/* Blog Card */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-0">
+      <BlogCard
+           image={blogPosts[0].image}
+           category={blogPosts[0].category}
+           title={blogPosts[0].title}
+           description={blogPosts[0].description}
+           author={blogPosts[0].author}
+           date={blogPosts[0].date}
+         />
+         <BlogCard
+           image={blogPosts[1].image}
+           category={blogPosts[1].category}
+           title={blogPosts[1].title}
+           description={blogPosts[1].description}
+           author={blogPosts[1].author}
+           date={blogPosts[1].date}
+         />
+         <BlogCard
+           image={blogPosts[2].image}
+           category={blogPosts[2].category}
+           title={blogPosts[2].title}
+           description={blogPosts[2].description}
+           author={blogPosts[2].author}
+           date={blogPosts[2].date}
+         />
+         <BlogCard
+           image={blogPosts[3].image}
+           category={blogPosts[3].category}
+           title={blogPosts[3].title}
+           description={blogPosts[3].description}
+           author={blogPosts[3].author}
+           date={blogPosts[3].date}
+         />
+         <BlogCard
+           image={blogPosts[4].image}
+           category={blogPosts[4].category}
+           title={blogPosts[4].title}
+           description={blogPosts[4].description}
+           author={blogPosts[4].author}
+           date={blogPosts[4].date}
+         />
+         <BlogCard
+           image={blogPosts[5].image}
+           category={blogPosts[5].category}
+           title={blogPosts[5].title}
+           description={blogPosts[5].description}
+           author={blogPosts[5].author}
+           date={blogPosts[5].date}
+         />
+      </div>
+
+
     </div>
   );
 }
