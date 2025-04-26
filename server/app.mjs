c1434455b06a0a3d4e0876065ea3d840cc619cb7
Tor <p.sripayom@gmail.com>
2025-04-26 15:37:37 +0700

    <feat>/api: add postsRouter with CRUD operations and pagination
    - Create postsRouter to handle APIs for posts table
    - Implement READ, UPDATE, DELETE endpoints
    - Support filtering by category and keyword in READ API
    - Add pagination to READ API

--- server/app.mjs
+++ server/app.mjs
@@ -1,8 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connectionPool from "./utils/db.js";
-import postValidate from "./middlewares/postValidate.mjs";
+import postsRouter from "./routes/postsRoutes.mjs";
 
 dotenv.config();
 
@@ -12,43 +11,7 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/profiles", (req, res) => {
-  return res.status(200).json({
-    data: {
-      name: "john",
-      age: 20,
-    },
-  });
-});
-
-app.post("/posts", [postValidate], async (req, res) => {
-  const { title, image, category_id, description, content, status_id } = req.body;
-  const date = new Date();
-
-  const insertQuery = `
-    INSERT INTO posts (title, image, category_id, description, content, status_id, date)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)
-  `;
-
-  const values = [title, image, category_id, description, content, status_id, date];
-
-  try{
-    const response = await connectionPool.query(insertQuery, values);
-    
-    if (response.rowCount === 1) {
-      return res.status(201).json({
-        message: "Created post sucessfully",
-      });
-    } else {
-      return res.status(400).json({
-        message: "Question was not created.",
-      });
-    }
-} catch (err) {
-    console.error(err);
-    return res.status(500).json({ error: "Server could not create post because database connection" });
-  }
-})  
+app.use("/posts", postsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on at ${PORT}`);
